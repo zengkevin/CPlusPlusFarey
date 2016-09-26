5504f5f7a00e4bd759856e56dfa3add78b32dff7
kvz <zeng.kevin@yahoo.com>
2016-09-26 16:23:08 -0700

    Init checkin

--- CPlusPlusFarey/CPlusPlusFarey.cpp
+++ CPlusPlusFarey/CPlusPlusFarey.cpp
@@ -2,10 +2,41 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
 
 int main()
 {
-    return 0;
+	int i;
+
+	while (true)
+	{
+		cin >> i;
+
+		if (i<=0) break;
+
+		cout << 0 << "/" << i << endl;
+
+		for (int m = 1; m <= i; m++)
+		{
+			int x = m, y = i;
+
+			for (int n = m; n >= 2; n--)
+			{
+				if (((m % n) == 0) && ((i % n) == 0))
+				{
+					x = m / n;
+					y = i / n;
+					break;
+				}
+			}
+
+			cout << x << "/" << y << endl;
+		}
+	}
+
+	return 0;
 }
 
