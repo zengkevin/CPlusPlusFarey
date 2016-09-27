49474c12b072c27ca4e3a3b6b4f6971be6f5adbc
kvz <zeng.kevin@yahoo.com>
2016-09-26 23:04:39 -0700

    Unit Test Passed

--- CPPUnitTest/CPPUnitTest.cpp
+++ CPPUnitTest/CPPUnitTest.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include "../FarceyGenerator/FareyGenerator.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -11,7 +12,28 @@ namespace CPPUnitTest
 		
 		TEST_METHOD(FareyGeneration)
 		{
-			// TODO: Your test code here
+			vector<fraction> fareySet;
+			vector<fraction> fareySet12 = {
+				{1, 12},
+				{1, 6}, 
+				{1, 4},
+				{1, 3},
+				{5, 12},
+				{1, 2},
+				{7, 12},
+				{2, 3},
+				{3, 4},
+				{5, 6},
+				{11, 12},
+				{1, 1},
+			};
+			FareyGenerator fareyGenerator;
+			fareyGenerator.FareyGeneration(12, fareySet);
+
+			for (vector<fraction>::size_type i = 0; i != fareySet.size(); i++) {
+				Assert::AreEqual(fareySet12[i].numerator, fareySet[i].numerator);
+				Assert::AreEqual(fareySet12[i].denominator, fareySet[i].denominator);
+			}
 		}
 
 	};
